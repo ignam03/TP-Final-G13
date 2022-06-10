bc6899291ea407cc03e42dec308b828e62eb76b3
PC <PC@DESKTOP-TVIBC5F>
2022-06-10 16:13:32 -0300

    controller

--- src/main/java/ar/edu/unju/fi/controller/IndexController.java
+++ src/main/java/ar/edu/unju/fi/controller/IndexController.java
@@ -6,13 +6,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/pci")
+@RequestMapping("/Empleo")
 public class IndexController {
     
 
     @GetMapping("/inicio")
     public String getInicioPage(Model model) {
         return "index";
+    }
+    @GetMapping("/login")
+    public String getLoginPage(Model model) {
+        return "loginForm";
+    }
+    @GetMapping("/login1")
+    public String getCiudadanoPage(Model model) {
+        return "ciudadanoForm";
+    }
+    @GetMapping("/login2")
+    public String getEmpleadorPage(Model model) {
+        return "empleadorForm";
     }
 
 }
